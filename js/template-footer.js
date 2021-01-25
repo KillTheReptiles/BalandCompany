1d90ec9194f73ccc2669958597b1406faeaa5a25
unknown <angelasofiaremolinagutierrez@gmail.com>
2021-01-24 19:06:35 -0500

    FAQ Section

---
+++ js/template-footer.js
@@ -0,0 +1,34 @@
+document.write(`
+
+<footer id="footer" data-stellar-background-ratio="0.5">
+        <div class="container">
+            <div class="row">
+
+                <div class="copyright-text col-md-12 col-sm-12">
+                    <div class="col-md-6 col-sm-6">
+                        <p>Copyright &copy; 2021 Baland Company</p>
+                    </div>
+
+                    <div class="col-md-6 col-sm-6">
+                        <ul class="social-icon">
+                            <li>
+                                <a href="https://www.instagram.com/thebalandart/" target="_blank" class="fa fa-instagram"></a>
+                            </li>
+                            <li>
+                                <a href="https://www.facebook.com/thebalandart" target="_blank" class="fa fa-facebook-square" attr="facebook icon"></a>
+                            </li>
+                            <li>
+                                <a href="https://api.whatsapp.com/send?phone=+573506266968" target="_blank" class="fa fa-whatsapp"></a>
+                            </li>
+                            <li>
+                                <a href="#soon" target="_blank" class="fa fa-twitter"></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+
+`);
---
+++ js/template-header.js
@@ -0,0 +1,48 @@
+document.write(`
+
+<section class="navbar custom-navbar navbar-fixed-top" role="navigation">
+        <div class="container">
+
+            <div class="navbar-header">
+                <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                    </button>
+
+                <!-- lOGO TEXT HERE -->
+                <a href="index.html" class="navbar-brand">Baland Designs</a>
+            </div>
+
+            <!-- MENU LINKS -->
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="index.html" class="smoothScroll">Home</a></li>
+                    <li><a href="portafolio.html" class="smoothScroll">Portafolio</a></li>
+                    <li><a href="https://balandcompany.com/#contact" class="smoothScroll">Contact</a></li>
+                    <li><a href="FAQ.html" class="smoothScroll">FAQ</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+
+
+                    <ul class="social-icon">
+                        <li>
+                            <a href="https://www.instagram.com/thebalandart/" target="_blank" class="fa fa-instagram"></a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/thebalandart" target="_blank" class="fa fa-facebook-square" attr="facebook icon"></a>
+                        </li>
+                        <li>
+                            <a href="https://api.whatsapp.com/send?phone=+573506266968" target="_blank" class="fa fa-whatsapp"></a>
+                        </li>
+                        <li>
+                            <a href="#soon" target="_blank" class="fa fa-twitter"></a>
+                        </li>
+                    </ul>
+                </ul>
+            </div>
+
+        </div>
+    </section>
+
+`);
